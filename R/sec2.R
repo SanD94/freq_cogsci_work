0377cd5a761445c48370d9f028bb1d90163db0a7
Safa Andac <8544658+SanD94@users.noreply.github.com>
2024-06-25 15:03:14 +0200

    start hypothesis testing

--- R/sec2.R
+++ R/sec2.R
@@ -87,3 +87,135 @@ hist(y_means,freq=FALSE,main="Sampling distribution of means \n (Data sampled \n
 abline(v=mean(y_means),lty=2)
 lines(seq(9,11,by=0.01),
       dnorm(seq(9,11,by=0.01),mean(y_means),sd(y_means)))
+
+#### 2. gamma distribution
+k<-2000
+y_matrix<-matrix(rep(NA,n*k),ncol=k)
+for(i in 1:k){
+  y_matrix[,i]<-rgamma(n,shape=1,rate=1)
+}
+
+op<-par(mfrow=c(1,2),pty="s")
+for(i in 1:k){
+  if(i==1){
+plot(density(y_matrix[,i]),main="Samples from a \n Gamma distribution",
+     xlab="")
+  } else {
+lines(density(y_matrix[,i]),lty=2)
+  }
+}
+## compute means from each replication:
+y_means<-colMeans(y_matrix)
+hist(y_means,freq=FALSE,main="Sampling distribution of means \n (Data sampled from Gamma)",
+     xlab="means under repeated sampling")
+abline(v=mean(y_means),lty=2)
+lines(seq(.9,1.1,by=0.01),
+      dnorm(seq(.9,1.1,by=0.01),mean(y_means),sd(y_means)))
+
+
+#### 3. cauchy distribution
+k<-2000
+y_matrix<-matrix(rep(NA,n*k),ncol=k)
+for(i in 1:k){
+  y_matrix[,i]<-rcauchy(n)
+}
+
+op<-par(mfrow=c(1,2),pty="s")
+for(i in 1:k){
+  if(i==1){
+plot(density(y_matrix[,i]),main="Samples from a \n Cauchy distribution",
+     xlab="")
+  } else {
+lines(density(y_matrix[,i]),lty=2)
+  }
+}
+## compute means from each replication:
+y_means<-colMeans(y_matrix)
+hist(y_means,freq=FALSE,main="Sampling distribution of means \n (Data sampled from Cauchy)",
+     xlab="means under repeated sampling")
+abline(v=median(y_means),lty=2)
+lines(seq(.9,1.1,by=0.01),
+      dnorm(seq(.9,1.1,by=0.01),mean(y_means),sd(y_means)))
+
+
+
+### Power calculation
+sds <- seq(100, 300, by = 1)
+lower <- power.t.test(
+  delta = 15 - 5,
+  sd = sds, n = 10,
+  strict = TRUE
+)$power
+upper <- power.t.test(
+  delta = 15 + 5,
+  sd = sds, n = 10,
+  strict = TRUE
+)$power
+meanval <- power.t.test(
+  delta = 15,
+  sd = sds, n = 10,
+  strict = TRUE
+)$power
+
+plot(sds, meanval,
+  type = "l",
+  main = "Power curve (n=10)\n
+     using power.t.test",
+  xlab = "standard deviation",
+  ylab = "power",
+  yaxs = "i",
+  ylim = c(0.05, 0.12)
+)
+lines(sds, lower, lty = 2)
+lines(sds, upper, lty = 2)
+text(125, 0.053, "10")
+text(150, 0.054, "15")
+text(175, 0.056, "20")
+
+compute_power <- function(nsim = 100000, n = 10,
+                          effect = NULL,
+                          stddev = NULL) {
+  crit_t <- abs(qt(0.025, df = n - 1))
+  temp_power <- rep(NA, nsim)
+  for (i in 1:nsim) {
+    y <- rnorm(n, mean = effect, sd = stddev)
+    temp_power[i] <- ifelse(abs(t.test(y)$statistic)
+    > crit_t, 1, 0)
+  }
+  ## return power calculation:
+  mean(temp_power)
+}
+
+sds <- seq(100, 300, by = 2)
+nsim <- 100000
+n <- 10
+power_meanval <- power_lowerval <- power_upperval <- rep(NA, length(sds))
+for (s in 1:length(sds)) {
+  power_meanval[s] <- compute_power(
+    nsim = nsim, n = n,
+    effect = 15,
+    stddev = sds[s]
+  )
+
+  power_lowerval[s] <- compute_power(
+    nsim = nsim, n = n,
+    effect = 10,
+    stddev = sds[s]
+  )
+
+  power_upperval[s] <- compute_power(
+    nsim = nsim, n = n,
+    effect = 20,
+    stddev = sds[s]
+  )
+}
+plot(sds, lowess(power_upperval)$y,
+  type = "l", lty = 2,
+  xlab = "standard deviation",
+  ylab = "power",
+  main = "Power curve (n=10) \n using simulation",
+  ylim = c(0.05, 0.12),
+  yaxs = "i"
+)
+lines(sds, lowess(power_lowerval)$y, lty = 2)
+lines(sds, lowess(power_meanval)$y)
