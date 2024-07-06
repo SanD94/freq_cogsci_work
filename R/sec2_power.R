8432d72f32d1385acb6e95595dd28699b00cd788
Safa Andac <8544658+SanD94@users.noreply.github.com>
2024-07-06 08:12:56 +0200

    finishes sec2

--- R/sec2_power.R
+++ R/sec2_power.R
@@ -1,6 +1,3 @@
-library(furrr)
-library(magrittr)
-
 condition <- c(rep(letters[1:2], 2), rep(letters[2:1], 2))
 item <- rep(1:4, 2)
 subj <- rep(1:2, each = 4)
@@ -175,16 +172,15 @@ text(40, 0.07, "10")
 text(50, 0.09, "15")
 text(60, 0.11, "20")
 
-library(furrr)
 compute_power <- function(nsim = 100000, n = 10,
                           effect = NULL,
                           stddev = NULL) {
   crit_t <- abs(qt(0.025, df = n - 1))
   temp_power <- future_map_dbl(rep(0, nsim), function(i) {
-                                 y <- rnorm(n, mean = effect, sd = stddev)
-                                 ifelse(abs(t.test(y)$statistic) > crit_t, 1, 0)
-                          },
-                          .options = furrr_options(seed = TRUE)
+    y <- rnorm(n, mean = effect, sd = stddev)
+    ifelse(abs(t.test(y)$statistic) > crit_t, 1, 0)
+  },
+    .options = furrr_options(seed = TRUE)
   )
   ## return power calculation:
   mean(temp_power)
@@ -195,15 +191,12 @@ sds <- seq(10, 300, by = 5)
 nsim <- 100000
 n <- 10
 power_meanval <- future_map_dbl(sds,
-                                ~compute_power(nsim = nsim, n = n,
-                                               effect = 15, .))
+    ~compute_power(nsim = nsim, n = n, effect = 15, .))
 power_lowerval <- future_map_dbl(sds,
-                                ~compute_power(nsim = nsim, n = n,
-                                               effect = 10, .))
+  ~compute_power(nsim = nsim, n = n, effect = 10, .))
 
 power_upperval <- future_map_dbl(sds,
-                                ~compute_power(nsim = nsim, n = n,
-                                               effect = 20, .))
+  ~compute_power(nsim = nsim, n = n, effect = 20, .))
 plot(sds, power_upperval,
   type = "l", lty = 2,
   xlab = "standard deviation",
@@ -225,11 +218,11 @@ N <- 37
 sd <- se * sqrt(N)
 nsim <- 10000
 drep <- future_map_dbl(rep(0, nsim),
-                       function(x) {
-                         samp <- rnorm(N, mean = D, sd = sd)
-                         mean(samp)
-                       },
-                       .options = furrr_options(seed = TRUE)
+  function(x) {
+    samp <- rnorm(N, mean = D, sd = sd)
+    mean(samp)
+  },
+  .options = furrr_options(seed = TRUE)
 )
 
 pow <- mean(ifelse(abs(drep / se) > 2, 1, 0))
@@ -248,19 +241,19 @@ truemu <- 15
 
 sampsize <- seq(10, 10000, by = 10)
 elem <- future_map(sampsize, function(n) {
-    y <- rnorm(n, mean = truemu, sd = 250)
-    sig <- ifelse(t.test(y)$p.value < 0.05, 1, 0)
-    means <- mean(y)
-    power <- power.t.test(d = truemu, sd = 250, n = n)$power
-    data.frame(sig, means, power)
+  y <- rnorm(n, mean = truemu, sd = 250)
+  sig <- ifelse(t.test(y)$p.value < 0.05, 1, 0)
+  means <- mean(y)
+  power <- power.t.test(d = truemu, sd = 250, n = n)$power
+  data.frame(sig, means, power)
 }, .options = furrr_options(seed = TRUE)) %>% list_rbind()
 
 
 plot(jitter(elem$means), jitter(elem$power),
-     main = "Funnel plot",
-     xlim = range(c(min(elem$means), max(elem$means))),
-     xlab = "effect", ylab = "power",
-     cex.lab = 1.8, cex.axis = 1.5, cex.main = 1.5
+  main = "Funnel plot",
+  xlim = range(c(min(elem$means), max(elem$means))),
+  xlab = "effect", ylab = "power",
+  cex.lab = 1.8, cex.axis = 1.5, cex.main = 1.5
 )
 abline(v = 15)
 elem_sig <- elem %>% dplyr::filter(sig == 1)
@@ -272,11 +265,11 @@ nsim <- 10000
 stddev <- 250
 mn <- 0
 res <- future_map(1:nsim, function(i) {
-    samp <- rnorm(n, mean = mn, sd = stddev)
-    test <- t.test(samp)
-    pvals <- test$p.value
-    t_stat <- test$statistic
-    data.frame(pvals, t_stat)
+  samp <- rnorm(n, mean = mn, sd = stddev)
+  test <- t.test(samp)
+  pvals <- test$p.value
+  t_stat <- test$statistic
+  data.frame(pvals, t_stat)
 }, .options = furrr_options(seed = TRUE)) %>% list_rbind()
 
 mean(res$pvals < 0.05) %>% round(2)
@@ -285,18 +278,18 @@ mean(res$pvals < 0.05) %>% round(2)
 ## how many subjects can I run?
 upper_bound <- 6 * n
 res_inc <- future_map(1:nsim, function(i) {
-    sig <- FALSE
-    x <- rnorm(n, mean = mn, sd = stddev)
-    while (!sig && length(x) < upper_bound) {
-      if (t.test(x)$p.value > 0.05)
-        x <- append(x, rnorm(n, mean = mn, sd = stddev))
-      else
-        sig <- TRUE
-    }
-    test <- t.test(x)
-    pvals <- test$p.value
-    t_stat <- test$statistic
-    data.frame(pvals, t_stat)
+  sig <- FALSE
+  x <- rnorm(n, mean = mn, sd = stddev)
+  while (!sig && length(x) < upper_bound) {
+    if (t.test(x)$p.value > 0.05)
+    x <- append(x, rnorm(n, mean = mn, sd = stddev))
+    else
+    sig <- TRUE
+  }
+  test <- t.test(x)
+  pvals <- test$p.value
+  t_stat <- test$statistic
+  data.frame(pvals, t_stat)
 }, .options = furrr_options(seed = TRUE)) %>% list_rbind()
 
 mean(res_inc$pvals < 0.05) %>% round(2)
